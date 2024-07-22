3b65b2025875031e15a8919c1b3f585f086d80a9
Francisco Wallison <franciscowallison@gmail.com>
2024-07-22 07:43:31 -0300

    ini alter web packt

--- webpack.config.js
+++ webpack.config.js
@@ -3,44 +3,46 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-	entry: "./index.js",
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "dist"),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-				},
-			},
-			{
-				test: /\.css$/, // Se você estiver usando CSS
-				use: ["style-loader", "css-loader"],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./index.html",
-		}),
-		new CopyWebpackPlugin({
-			patterns: [{ from: "src", to: "src" }],
-		}),
-	],
-	devServer: {
-		static: {
-			directory: path.join(__dirname, "dist"),
-		},
-		compress: true,
-		port: 8989,
-	},
-	resolve: {
-		alias: {
-			// Adicione aliases se necessário para facilitar a migração dos caminhos AMD para ES6
-		},
-	},
+    entry: "./index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+            },
+            {
+                test: /\.css$/, // Se você estiver usando CSS
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{ from: "src", to: "src" }, { from: "node_modules", to: "node_modules" }],
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        compress: true,
+        port: 8989,
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
+        extensions: ['.js'],
+    },
 };
